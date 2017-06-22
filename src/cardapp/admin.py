df1a79dae2d6ca0e2ec5980a0cc6a9d4edcff746
Raphael Velt <raph.velt@gmail.com>
2017-06-22 17:32:38 +0100

    Small improvements

--- src/cardapp/admin.py
+++ src/cardapp/admin.py
@@ -9,13 +9,9 @@ def make_public(modeladmin, request, queryset):
 def make_private(modeladmin, request, queryset):
     queryset.update(public=False)
 
-class TaggedAdmin(admin.ModelAdmin):
-    
-    def get_queryset(self, request):
-        return super(TaggedAdmin, self).get_queryset(request).prefetch_related('tags')
 
-    def tag_list(self, obj):
-        return ", ".join(o.name for o in obj.tags.all())
+def tag_list(obj):
+    return ", ".join(o.name for o in obj.tags.all())
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
@@ -29,24 +25,29 @@ class TagAdmin(admin.ModelAdmin):
         )
     
 @admin.register(Deck)
-class DeckAdmin(TaggedAdmin):
+class DeckAdmin(admin.ModelAdmin):
     actions = (make_public, make_private)
-    list_display = ('__str__', 'author', 'public', 'card_count', 'tag_list')
+    list_display = ('__str__', 'author', 'public', 'card_count', tag_list, 'admin_thumbnail')
+    readonly_fields = ('admin_thumbnail',)
     list_filter = ('author', 'public')
+    admin_thumbnail = AdminThumbnail(image_field='thumbnail')
     
     def card_count(self, obj):
         return obj.cards.count()
 
     def get_queryset(self, request):
-        return super(DeckAdmin, self).get_queryset(request).prefetch_related('cards')
+        return super(DeckAdmin, self).get_queryset(request).prefetch_related('cards', 'tags')
 
 @admin.register(Card)
-class CardAdmin(TaggedAdmin):
+class CardAdmin(admin.ModelAdmin):
     actions = (make_public, make_private)
-    list_display = ('__str__', 'author', 'public', 'admin_thumbnail', 'deck', 'tag_list')
+    list_display = ('__str__', 'author', 'public', 'deck', tag_list, 'admin_thumbnail')
     readonly_fields = ('admin_thumbnail',)
     list_filter = ('author', 'public', 'deck')
     admin_thumbnail = AdminThumbnail(image_field='thumbnail')
+    
+    def get_queryset(self, request):
+        return super(CardAdmin, self).get_queryset(request).prefetch_related('tags')
 
 class CardOnCardMapInline(admin.TabularInline):
     model = CardOnCardMap
@@ -57,9 +58,9 @@ class AnnotationInline(admin.TabularInline):
     extra = 1
 
 @admin.register(CardMap)
-class CardMapAdmin(TaggedAdmin):
+class CardMapAdmin(admin.ModelAdmin):
     actions = (make_public, make_private)
-    list_display = ('__str__', 'author', 'public', 'admin_thumbnail', 'deck', 'card_count', 'annotation_count', 'tag_list')
+    list_display = ('__str__', 'author', 'public', 'deck', 'card_count', 'annotation_count', tag_list, 'admin_thumbnail')
     readonly_fields = ('admin_thumbnail',)
     list_filter = ('author', 'public', 'deck')
     admin_thumbnail = AdminThumbnail(image_field='thumbnail')
@@ -72,4 +73,4 @@ class CardMapAdmin(TaggedAdmin):
         return obj.annotationoncardmap_set.count()
     
     def get_queryset(self, request):
-        return super(CardMapAdmin, self).get_queryset(request).prefetch_related('annotationoncardmap_set', 'cardoncardmap_set')
+        return super(CardMapAdmin, self).get_queryset(request).prefetch_related('annotationoncardmap_set', 'cardoncardmap_set', 'tags')
